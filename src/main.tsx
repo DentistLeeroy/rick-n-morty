538749dff3f48329afbc97253dce780cecfaba47
DentistLeeroy <74152951+DentistLeeroy@users.noreply.github.com>
2023-02-23 09:48:39 +0100

    query setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <ApolloProvider client={client}>1
+    <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
     </BrowserRouter>
--- src/pages/Charecters.tsx
+++ src/pages/Charecters.tsx
@@ -1,11 +1,33 @@
 import styles from "./Charecters.module.css"
 import LogoTitle from "../assets/logo-outline.png"
 import Card from "../components/library/visual/Card";
+import {useQuery, gql} from '@apollo/client';
 
 export default function Charecters() {
 
-
-
+    const CHARACTERS_QUERY = gql `
+    query allCharacters {
+        characters {
+          results {
+            name
+            species
+            status
+            image
+            gender
+            type
+            origin {
+              name
+            }
+            location {
+              name
+            }
+          }
+        }
+      }
+    ` 
+    const { loading, error, data } = useQuery(CHARACTERS_QUERY);
+    console.log(data);
+    
 
     return(
         <>
